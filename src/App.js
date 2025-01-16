4de51e896bc0790e431422666bcd0f8dbe8d9cde
Kanriver <Kannatee2546kan@gmail.com>
2025-01-16 14:04:12 +0700

    Add refs-func.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { EventData2 } from './event-data'
+import RefFunc from './refs-func'
 
 export default function App() {
-  return <EventData2/>
+  return <RefFunc/>
 }
